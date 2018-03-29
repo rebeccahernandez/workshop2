bead17f75145623233bba48dcc07025358543d87
rebeccahernandez <35234693+rebeccahernandez@users.noreply.github.com>
2018-03-28 22:44:13 -0400

    Add files via upload

---
+++ comp1/scripts/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+	for (i=1;i<50;i++){
+		$('.wide-box').append('<div class="stripe"></div>')
+	}
+
+	for (i=1;i<101;i++){
+		$('#col-3').append('<div class="stripe"></div>')
+	}
+
+	for (i=1;i<51;i++){
+		$('#col-1').append('<div class="stripe"></div>')
+	}
+
+	for (i=1;i<100;i++){
+		$('#col-2').append('<div class="stripe"></div>')
+	}
+
+});
